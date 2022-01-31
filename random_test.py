a8d2d42564090855d0e9e1c873bdda2ac6180c5e
Kouki Yanai <e205702@ie.u-ryukyu.ac.jp>
2022-02-01 05:28:31 +0900

    1st commit

---
+++ random_test.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+def get_random_password_string(length):
+    pass_chars = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(pass_chars) for x in range(length))
+    return password
+
+print(get_random_password_string(13))
